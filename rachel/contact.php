8aa8c932ba7827c884267b152e7f49689488fc4c
planglois7 <pnl@uoregon.edu>
2017-09-05 16:42:38 +0200

    Update contact.php

--- rachel/contact.php
+++ rachel/contact.php
@@ -1,16 +1,10 @@
 <?php 
-if(!isset($_POST['submit']))
-{
-echo "error; you need to submit the form!;
-}
+
 $name = $_POST['sender'];
 $visitor_email = $_POST['senderEmail'];
 $message = $_POST['message'];
 
-if(empty($name)||empty($visitor_email))
-{
-echo "name and email are mandatory!;
-}
+
 
 $email_from = 'perilanglois7@gmail.com';
 $email_subject = "new form";
